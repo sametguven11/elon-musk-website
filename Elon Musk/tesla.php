c1a80844903b5cae72f65332c21e6232d85dc565
sametguven11 <samettguven11@gmail.com>
2023-12-25 16:01:36 +0300

    elon-musk-website

---
+++ Elon Musk/tesla.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elon Musk</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+<body>
+ <div class="containert">
+     <div class="navbar">
+         <div class="logo">
+            <a href="index.html"><img src="dogecoin-cryptocurrency-digital-currency-doge.jpg"></a>
+             
+         </div>
+         <ul>
+             <li><a href="index.html" >Ana Sayfa</a></li>
+             <li><a href="#" class="active">Tesla</a></li>
+             <li><a href="spacex.html">SpaceX</a></li>
+             <li><a href="kripto.html">Borsa</a></li>
+             <li><a href="starlink.html">Starlink</a></li>
+         </ul>
+     </div>
+     <div class="box">
+         <img src="39-396698_tesla-logo-[eps-motors]-tesla-logo-icon.png" >
+         <h1>Tesla'nın Kuruluşu</h1>
+         <p>Tesla, eski adıyla “Tesla Motors” 2003 yılında iki Amerikalı girişimci olan Martin Eberhard ve Marc Tarpenning tarafından kurulmuştur. Tesla Motors isimli bu şirket 2017 yılında Tesla adını almıştır. Tesla şirketinin temel amacı elektrikli otomobil üretmektir ve 16 yıldır bu alanda çok büyük adımlar atmıştır. Tesla şirketi adını Nikola Tesla’dan almıştır.
+        Tesla şirketi kurulduğunda amaçları elektrikli spor araba üretmekti. Martin Eberhard ve Marc Tarpenning bu şirket için yatırım arayışına girmişlerdi. Marc Tarpenning mali işlerden sorumluydu ve Martin Eberhand ise yöneticisiydi. Çeşitli kaynaklardan yatırım elde eden Tesla şirketi, en büyük yatırımını Paypal’ın kurucu ortaklarından biri olan Elon Musk’dan almıştır. 2004 yılında yapılan bu yatırımın değeri 30 milyon dolar kadardı ve bu yatırımdan sonra Elon Musk, şirketin başına geçmiştir.</p>
+        <ul>
+            <li><a href="https://twitter.com/tesla" target="_blank"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/teslamotors/" target="_blank"><i class="fab fa-instagram"></i></a></li>
+         </ul>
+     </div>
+     <div class="teslax">
+          <h1>Şahin kanatları ile elektrikli SUV Tesla Model X:</h1>
+          <p>Tesla Model X için elektrikli araçların zirvesi ifadesini kullanabiliriz. Sahip olduğu özellikler ve fiyatı da bu söylemin doğruluğunu kanıtlar niteliğinde. 7 kişilik oturma kapasitesine sahip elektrikli otomotiv Tesla Model X tasarımın büyük bir kısmını(özellikle iç tasarımını)  Model S’e borçlu.</p>
+          <p>Aracın tartışmasız en dikkat çeken özelliklerinden biri de “falcon wings” diye adlandırılan yukarıya açılır elektronik kapıları. Tesla’nın tasarımcıları bu kapıları,1985 yılında vizyona giren Amerikan bilim kurgu filmi Geleceğe Dönüş’ten esinlenerek tasarladıklarını belirtiyor.</p>
+          <p>Tesla’nın özellikle üzerinde durduğu tam otonom sürüş teknolojisini kullanmak istiyorsanız her modelde olduğu gibi bu modelde de ek ücret ödemeniz gerekyior. Eğer Model X otomobilinizde tam otonom bir sürüş keyfi sürmek istiyorsanız 10 bin dolar daha ödeme yapmanız gerekiyor. Ancak otomatik pilot özelliği için ücret ödemiyorsunuz Tesla bu özelliği standart olarak sunuyor. </p>
+           <img src="teslaX.jpeg" >
+     </div>
+     <div class="teslas">
+         <h1>Spor elektrikli otomobil: Tesla Model S:</h1>
+         <p>Tesla Model S, bir Jaguar'da bulabileceğiniz benzerlikte, uzun ve alçak tasarım çizgilere sahip, son derece donanımlı bir sedan modeldir.</p>
+         <p>Model S önceki sürümlerinden bazılarında arkadan çekiş prensibiyle çalışırken, son üretilen Model S’lerin tümü dört tekerlekten çekişli olarak çalışıyor. Bu da Model S’i performans açısından daha da güçlü kılıyor. Performans konusunda fazlasıyla iyi olan Model S’in Plaid versiyonu inanması güç olsa da tam 1020 beygir gücünde.</p>
+         <p>Model S ilk olarak 2012’de piyasaya sürüldü ve Tesla’nın bir otomobil üreticisi olarak piyasaya girmesine yardımcı oldu. Tesla, görücüye çıktığından beri Model S’yi güncellemeye devam etti ve son olarak şuan Model S’in üç versiyonu bulunuyor: Long Range Plus; Ekose; ve Plaid+. Standart ve Performans modelleri kısa süre önce ürün gamından çıkarıldı.</p>
+         <p>Tesla’nın ürettiği en performanslı otomobili olan Model S, hız tutkunlarının beğeneceği türden 0-100 performansı verirken aynı zamanda çok iyi batarya menzillerine de sahip:</p>
+         <img src="teslaS.jpeg" >
+     </div>
+     <div class="teslay">
+         <h1>Devlerle yarışan otomobil ve SUV hibriti: Tesla Model Y</h1>
+         <p>Tesla Model Y tam anlamıyla Tesla’nın kompakt SUV’u olarak geçiyor. Çünkü diğer SUV model olan Model X’e göre Model Y hem daha küçük ebatlarda hem de daha ekonomik. Yani aslında Model Y Tesla’nın en ucuz modeli Model 3 ile en donanımlı aracı Model X arasındaki boşluğu dolduruyor. Aynı zamanda dilerseniz 3 bin dolar daha ödeyerek aracınıza 2 koltuk daha ekleyerek kapasiteyi 7 kişiye çıkarabilirsiniz.</p>
+         <p>Kompakt SUV segmenti günümüzde en çok satan segmentlerdendir ve elektrikli seçeneği yok denecek kadar azdır. Bu yüzden Tesla Model Y ile elini rakiplerine karşı bir hayli güçlendirmiştir. Model X ve Model S’ye göre daha yavaş bir otomobil olsa da performans versiyon Model Y’nin 0-100 performansı, Bentley’in SUV modeli 626 beygir gücünde Bentayga’dan bile daha iyi. Tesla Model Y’nin batarya menzili ve 0-100km/s süresi işte şu şekilde:</p>
+         <img src="teslaY.jpeg"> 
+     </div>
+     <div class="tesla3">
+         <h1>Ailenin en ucuzu: Tesla Model</h1>
+         <p>Tesla’nın ürettiği en ekonomik ve en hafif olan aracı Tesla Model 3, 2016 yılında Amerika’da Model S’ye uygun fiyatlı bir alternatif olarak piyasaya sürüldü ve güncel fiyatıyla Tesla’nın sattığı en ucuz otomobil.</p>
+         <p>Dört kapılı bir sedan olan Model 3, bir dizi seçenekle piyasaya sürüldü, ancak şu an sadece üç farklı seçenekle satılacak şekilde düzenlendi. Diğer modellerde de olduğu gibi otomatik pilot Model 3’te de standart ancak tam otonom sürüş özelliğini aktif etmek istediğinizde 10 bin dolar daha gözden çıkarmanız gerekiyor.</p>
+         <img src="tesla3.jpeg" >
+         
+        
+         </div>
+
+     </div>
+     
+
+</body>
+</html>
